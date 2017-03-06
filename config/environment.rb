e77bad04d912e9377917b4f26309c84c4c61906b
Steve Longoria <stevehl25@gmail.com>
2017-03-06 00:08:34 +0000

    Revised environments file

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'shielded-beyond-30706.herokuapp.com/',
+  :domain         => 'shielded-beyond-30706.herokuapp.com',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
